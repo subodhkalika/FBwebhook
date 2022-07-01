12c5ea30a62855571aae1e9ed8aa81d52a11718b
Subodh Kalika <kalikasubodh@gmail.com>
2022-07-01 16:52:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser');
 require('dotenv').config();
 const port = process.env.PORT || 3023
 
-app.use(express.json());
+app.use(express.json({
+  verify: (req, res, buf) => {
+      req.rawBody = buf
+  }
+}));
+
+const crypto = require('crypto');
 
 /* 
     Incase you are using mongodb atlas database uncomment below line
@@ -25,6 +32,24 @@ app.post('/webhook', (req, res) => {
   console.log("headers", req.headers);
   console.log("body");
   console.dir(req.body, {depth: null});
+  console.log("rawBody", req.rawBody)
+  const calculatedChecksum = crypto.createHmac('sha1', process.env.APP_SECRET)
+                            .update(req.rawBody)
+                            .digest('hex');
+  console.log({calculatedChecksum})
+
+  const calculatedChecksum256 = crypto.createHmac('sha256', process.env.APP_SECRET)
+                              .update(req.rawBody)
+                              .digest('hex');
+  console.log({calculatedChecksum256})
+
+
+  var buf = Buffer.from(JSON.stringify(req.body));
+  const calculatedChecksumBuf = crypto.createHmac('sha256', process.env.APP_SECRET)
+                                  .update(buf)
+                                  .digest('hex');
+  console.log(calculatedChecksumBuf);
+
   res.status(200).send('Ok!');
 })
 
